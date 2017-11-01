48ad275e0aeafcc1dff1d0e91392f3688ed25863
Nick Lorenson <n.e.lorenson@gmail.com>
2017-11-01 15:43:30 -0500

    Db setup

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const exp = require('express')
 const app = exp()
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const db = low(new FileSync('db.json'))
+
+db.defaults({
+  restaurants: [
+    {
+      name: 'Five Points Pizza',
+      tags: ['pizza']
+    }
+  ],
+
+  tags: {
+    pizza: ['Five Points Pizza']
+  },
+
+  users: []
+}).write()
+
 const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
-  return res.json({ msg: 'hello' })
+  const restaurants = db.get('restaurants').value()
+  return res.json({ msg: 'hello', restaurants })
 })
 
 app.listen(port, () => {
