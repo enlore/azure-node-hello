25fb8735efdb6539f28ed19d2a471f2fb6424906
Nick Lorenson <n.e.lorenson@gmail.com>
2017-11-02 19:10:42 -0500

    Fav path

--- app.js
+++ app.js
@@ -1,42 +1,70 @@
+const DEBUG = false
 const { resolve } = require('path')
+const uuid = require('uuid')
 
 const exp = require('express')
 const app = exp()
 const bod = require('body-parser')
+const cki = require('cookie-parser')
 
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
+const auth = require('./auth')
 
-const db = low(new FileSync('db.json'))
-
+const db = require('./db')
 const Restaurant = require('./models/Restaurant.js')
 
-db.defaults({
-  restaurants: [
-    {
-      name: 'Five Points Pizza',
-      tags: ['pizza']
-    }
-  ],
-
-  tags: {
-    pizza: ['Five Points Pizza']
-  },
-
-  users: []
-}).write()
+function log (req, res, next) { console.info(`request ${req.method} ${req.path}`); next() }
 
 const port = process.env.PORT || 8080
 
+// cookies
+app.use(cki())
+app.use(auth.session(db))
+
+// deserialize json POST bodies
 app.use(bod.json())
 
-app.get('/', (req, res) => {
-  return res.sendFile(resolve(__dirname, 'client', 'index.html'))
-})
+// talk about your day
+app.use(log)
+
+app.get('/', (req, res) => { return res.sendFile(resolve(__dirname, 'client', 'index.html')) })
+app.post('/api/secret', auth.secret(db))
 
 app.get('/api/restaurants', (req, res) => {
   const restaurants = db.get('restaurants').value()
   return res.json({ msg: 'hello', restaurants })
+})
+
+// do i know you?
+app.use(auth.protect) // hereafter are protected routes
+
+app.get('/api/favs', (req, res) => {
+  return res.json({ favorites: req.user.favorites })
+})
+
+app.post('/api/favs', (req, res) => {
+  let user = req.user
+  let rid = req.body.restaurantId
+
+  // if this is already a fav, unfav
+  if (user.favorites.some(f => f === rid)) {
+    let idx = user.favorites.indexOf(rid)
+
+    user.favorites.splice(idx, 1)
+
+    req.user = user // being lazy and not forcing refetch
+
+    // else fav it
+  } else {
+    user.favorites.push(rid)
+  }
+
+  // now update the user
+  db.get('users')
+    .find(u => u.id === user.id)
+    .assign(user)
+    .write()
+
+  return res.json({ statusCode: 200, message: 'Fav toggled' })
 })
 
 app.post('/api/restaurants', (req, res) => {
@@ -58,7 +86,8 @@ app.post('/api/restaurants', (req, res) => {
   let r = new Restaurant({
     name: ro.name,
     address: ro.address,
-    tags: ro.tags
+    tags: ro.tags,
+    uploader: req.user.id
   })
 
   r.save(db)
@@ -67,5 +96,5 @@ app.post('/api/restaurants', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.info(`>>>>>> Listening on ${port}`)
+  console.info(`>>>>>> Listening on http://localhost:${port}`)
 })
